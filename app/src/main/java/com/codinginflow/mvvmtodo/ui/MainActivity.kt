08a7d5f9821f3b30f77ea5e2707e4ee4bf88ba20
Jarno02 <jarno.ryhanen02@gmail.com>
2022-03-21 16:05:54 +0200

    Created the logic in AddEditTaskViewModel where the user can now create and update tasks Created code in AddEditTaskFragment and TasksFragment to forward the Snackbar request to their viewmodels minor changes

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/MainActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.codinginflow.mvvmtodo.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -22,10 +22,13 @@ class MainActivity : AppCompatActivity() {
          supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
-     setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
+
+const val ADD_TASK_RESULT_OK = Activity.RESULT_FIRST_USER
+const val EDIT_TASK_RESULT_OK = Activity.RESULT_FIRST_USER + 1
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -60,6 +61,11 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
             }
         }
 
+        setFragmentResultListener("add_edit_request") { _, bundle ->
+            val result = bundle.getInt("add_edit_result")
+            viewModel.onAddEditResult(result)
+        }
+
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
         }
@@ -87,6 +93,9 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
                                 "Edit task"
                             )
                         findNavController().navigate(action)
+                    }
+                    is TasksViewModel.TasksEvent.ShowTaskSavedConfirmationMessage -> {
+                        Snackbar.make(requireView(), event.msg, Snackbar.LENGTH_SHORT).show()
                     }
                 }
 
